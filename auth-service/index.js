ee1d8e9354491071418e8eeed1ce2f4b56e2ce56
Santiago Cañas Zapata <santiagozapata07@gmail.com>
2025-11-01 03:28:59 -0500

    final verifications to deploy

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,33 +1,29 @@
+import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import usersRouter from "./routes/users.routes.js";
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-// Middleware para manejar JSON
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api/users", usersRouter);
 
-// Conexión a MongoDB
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
-    console.log("✅ Conectado a MongoDB Atlas");
+    console.log("Conectado a MongoDB Atlas");
     app.listen(PORT, () => {
-      console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
-    console.error("❌ Error conectando a MongoDB:", err);
+    console.error("Error conectando a MongoDB:", err);
   });
 
-// Ruta base de prueba
 app.get("/", (req, res) => {
-  res.json({ message: "Backend funcionando ✅" });
+  res.json({ message: "Backend funcionando" });
 });
