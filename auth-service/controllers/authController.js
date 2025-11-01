fc81217bd6604806d769defbf2485d46d8bffdde
caszofficial <santiagozapata07@gmail.com>
2025-11-01 01:54:54 -0500

    auth service folder strutured

--- auth-service/controllers/authController.js
+++ auth-service/controllers/authController.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import User from "../models/User.js";
+import User from "../../models/User.js";
 
 // POST /api/users/register
 export const registerUser = async (req, res) => {
